450c02aafd4cdc55ce04c5482b434f83003c804d
qkdreyer <quentin.dreyer@triotech.fr>
2017-12-08 19:01:31 +0100

    Moved postdeploy script in build folder + Enhanced build logs

---
+++ build/postdeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Starting Postdeploy Script :"
+echo "- Building app..."
+node node_modules/@triotech/vue-core/build/build.js
+echo "- Copying .web in tmp..."
+cp -R .web tmp
+echo "- Moving web to old..."
+mv web old
+echo "- Moving tmp to web..."
+mv tmp web
+echo "- Removing old..."
+rm -rf old
+echo "Successfully ran Postdeploy script !"
