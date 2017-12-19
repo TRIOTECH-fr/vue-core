c625962999ddd0732b1012df3625b9e4461889d9
qkdreyer <quentin.dreyer@triotech.fr>
2017-12-19 14:32:56 +0100

    Added Logout component

---
+++ src/components/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logout">
+    <a @click="logout" href="#" class="float-right" v-html="text"></a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LogoutPage',
+    props: {
+      text: { type: String, default() { return this.$t('actions.logout'); } },
+      route: { type: String, default: 'home' },
+    },
+    methods: {
+      logout() {
+        this.$store.replaceState({});
+        this.$router.push({ name: this.route });
+      },
+    },
+  };
+</script>
