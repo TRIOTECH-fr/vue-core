9128f4490f1f954ff3f8777ac02461db8e188b1a
rcampos <rebecca.campos@triotech.fr>
2018-01-03 16:55:49 +0100

    Create vue multiselect component

--- src/vendor/vue-multiselect.js
+++ src/vendor/vue-multiselect.js
@@ -1,1 +1,23 @@
+import Vue from 'vue';
 import 'vue-multiselect/dist/vue-multiselect.min.css';
+import Multiselect from 'vue-multiselect';
+
+Vue.component('multiselect', {
+  functional: true,
+  render (h, context) {
+    console.log(context);
+    return h( Multiselect, {
+      props: {
+        options: context.props.options,
+        placeholder: context.parent.$t(`placeholders.${context.props.id}.${context.props.multiple ? 'multiple' : 'single'}`),
+        label: context.props.label,
+        // trackBy: context.props.value,
+        multiple: context.props.multiple,
+        selectLabel: '',
+        selectedLabel: '',
+        deselectLabel: '',
+        preserveSearch: true,
+      },
+    });
+  },
+});
