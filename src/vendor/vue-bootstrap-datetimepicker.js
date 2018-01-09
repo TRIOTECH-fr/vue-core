e3c0ae6751f282f1ba9b69d74049409e5a996661
Teddy ANDREOTTI <teddy.andreotti@triotech.fr>
2018-01-09 16:14:07 +0100

    Test datepicker

--- src/vendor/vue-bootstrap-datetimepicker.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import datePicker from 'vue-bootstrap-datetimepicker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css';
-
-Vue.use(datePicker);
---
+++ src/vendor/vuejs-datepicker.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Datepicker from 'vuejs-datepicker';
+
+Vue.component('my-component', {
+  components: {
+    Datepicker,
+  },
+});
