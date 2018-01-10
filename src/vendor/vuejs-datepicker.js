35d3c3e39a87e8be04e2bcd30f10d0e0418c633d
Teddy ANDREOTTI <teddy.andreotti@triotech.fr>
2018-01-10 17:01:44 +0100

    WIP vuejs-datepicker

--- src/vendor/vuejs-datepicker.js
+++ src/vendor/vuejs-datepicker.js
@@ -2,14 +2,28 @@ import Vue from 'vue';
 import Datepicker from 'vuejs-datepicker';
 import VueFormGenerator from 'vue-form-generator';
 
-delete Datepicker.props.value;
-
 Vue.component('fieldDatePicker', {
-  ...Datepicker,
+  template: '<datepicker :placeholder="schema.placeholder" v-model="datePicker_model"></datepicker>',
+  components: {
+    Datepicker,
+  },
+  data: function() {
+    return {
+      datePicker_model: '',
+    };
+  },
   mixins: [VueFormGenerator.abstractField],
   mounted() {
-    this.$on('input', (date) => {
-      this.value = this.$moment(date).format('DD-MM-YYYY');
-    });
+    this.datePicker_model = this.schema.default;
+  },
+  computed: {
+    value: {
+      get: function() {
+        return this.$moment(this.datePicker_model).format('DD-MM-YY')
+      },
+      set: function(newValue) {
+        // TODO converte and set value ! this.datePucker_model = newValue;
+      },
+    },
   },
 });
