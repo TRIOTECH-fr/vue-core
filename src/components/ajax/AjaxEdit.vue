d8d5f2feeaa58956ea70413945744eb7913cc874
Teddy ANDREOTTI <teddy.andreotti@triotech.fr>
2018-01-15 17:06:56 +0100

    Add AjaxIndex, AjaxNew, AjaxEdit ...

---
+++ src/components/ajax/AjaxEdit.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <form @submit.prevent="submit">
+      <template v-if="this.schema.fields.length > 0">
+        <vue-form-generator :schema="schema" :model="model" :options="{ validationAfterLoad: true, validationAfterChanged: true }" />
+        <b-row>
+          <b-col>
+            <b-button block :to="{ name: 'parameter.index' }">{{ $t('actions.go_back') }}</b-button>
+          </b-col>
+          <b-col>
+            <b-button block type="submit" variant="primary">{{ $t('actions.edit') }}</b-button>
+          </b-col>
+        </b-row>
+      </template>
+      <div class="text-center" v-else>
+        <i class="ti ti-2x ti-spin ti-refresh"></i>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import Ajax from '@triotech/vue-core/src/lib/http/ajax';
+  import VueFormGenerator from '@triotech/vue-core/src/bind/vue-form-generator';
+
+  export default {
+    name: 'AjaxEditComponent',
+    components: {
+      'vue-form-generator': VueFormGenerator.component,
+    },
+    data() {
+      return {
+        model_back: {},
+        model: {},
+        schema: {
+          fields: [],
+        },
+      };
+    },
+    props: {
+      id: null,
+      name: {
+        type: String,
+      },
+      ajaxTableRef: {
+        type: Object,
+        default: null,
+      },
+      refreshAjaxTable: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    async mounted() {
+      await Ajax.get(`${this.name}/${this.id}/edit`)
+        .then((data) => {
+          this.schema.fields = this.schema.fields.concat(_.form(this.$t, data.form));
+          this.model = data.entity;
+          this.model_back = JSON.parse(JSON.stringify(this.model));
+        })
+      ;
+    },
+    methods: {
+      async submit() {
+        const submitData = Ajax.difference(this.model, this.model_back);
+        if (!_.isEmpty(submitData)) {
+          await Ajax.patch(`${this.name}/${this.id}/edit`, submitData)
+            .then((data) => {
+              if (data.status) {
+                this.$notify({
+                  title: this.$t(`flashes.${name}.edit_title`),
+                  text: this.$t(`flashes.${name}.edit`),
+                  type: 'success',
+                });
+                if (this.$parent.$refs.content.className === 'sweet-content') {
+                  this.$parent.close();
+                }
+              } else {
+                this.$notify({
+                  title: this.$t(`flashes.${name}.edit_title`),
+                  text: this.$t(`flashes.${name}.not_edit`),
+                  type: 'error',
+                });
+              }
+
+              if (this.refreshAjaxTable) {
+                this.ajaxTableRef.refresh();
+              }
+            }, (errors) => {
+              if (errors.response.status === 400) {
+                this.$notify({
+                  title: this.$t(`flashes.${name}.request_error_title`),
+                  text: errors.response.data.errors.errors[0],
+                  type: 'warning',
+                });
+              }
+            })
+          ;
+        }
+        // todo close modal ? or do something !
+        return false;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- src/components/ajax/AjaxIndex.vue
+++ src/components/ajax/AjaxIndex.vue
@@ -1,38 +1,44 @@
 <template>
-  <b-col cols="12">
-    <div v-if="loader && isLoading" class="text-center mb-3">
-      <slot name="loader">
-        <i class="ti ti-2x ti-spin ti-refresh"></i>
-      </slot>
-    </div>
-    <template v-else>
-      <template v-if="items.length > 0">
-        <table class="table">
-          <slot name="header">
-            <tr>
-              <th>{{ $t('table.item') }}</th>
-              <th>{{ $t('table.index') }}</th>
-            </tr>
-          </slot>
-          <slot name="item" v-for="(item, index) in items" :item="item" :index="index">
-            <tr>
-              <td>{{ item }}</td>
-              <td>{{ index }}</td>
-            </tr>
-          </slot>
-        </table>
-      </template>
-      <b-alert v-else show>{{ $t(entityName + '.empty_set') }}</b-alert>
-      <slot name="footer"></slot>
-    </template>
-  </b-col>
+    <b-col cols="12">
+        <div v-if="loader && isLoading" class="text-center mb-3">
+            <slot name="loader">
+                <i class="ti ti-2x ti-spin ti-refresh"></i>
+            </slot>
+        </div>
+        <template v-else>
+            <template v-if="items.length > 0">
+                <table class="table" v-if="renderMode === 'table'">
+                    <slot name="header">
+                        <tr>
+                            <th>{{ $t('table.item') }}</th>
+                            <th>{{ $t('table.index') }}</th>
+                        </tr>
+                    </slot>
+                    <slot name="item" v-for="(item, index) in items" :item="item" :index="index">
+                        <tr>
+                            <td>{{ item }}</td>
+                            <td>{{ index }}</td>
+                        </tr>
+                    </slot>
+                </table>
+                <b-list-group v-else-if="renderMode === 'list'">
+                    <slot name="item" v-for="(item, index) in items" :item="item" :index="index">
+                        <b-list-group-item>{{ item }} - {{ index }}</b-list-group-item>
+                    </slot>
+                </b-list-group>
+                <!-- TODO render custom, render slot -->
+            </template>
+            <b-alert v-else show>{{ $t(entityName + '.empty_set') }}</b-alert>
+            <slot name="footer"></slot>
+        </template>
+    </b-col>
 </template>
 
 <script>
   import Ajax from '@triotech/vue-core/src/lib/http/ajax';
 
   export default {
-    name: 'AjaxTable',
+    name: 'AjaxIndex',
     props: {
       entityName: { type: String, default: 'item' },
       uri: { type: String, required: true },
@@ -44,6 +50,7 @@
       authPrefix: { type: String, default: 'Bearer' },
       loader: { type: Boolean, default: true },
       initLoad: { type: Boolean, default: true },
+      renderMode: { type: String, default: 'table' },
     },
     data() {
       return {
