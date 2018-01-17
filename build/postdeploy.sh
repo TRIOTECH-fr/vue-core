016abc1c11f52b232381f67f021b5ad72248fae7
qkdreyer <quentin.dreyer@triotech.fr>
2018-01-17 16:00:29 +0100

    Copying parameters.yml.dist before building application

--- build/postdeploy.sh
+++ build/postdeploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 clear
 echo "Starting Postdeploy Script :"
+#TODO instead of simple copy, handle differences
+echo "- Copying parameters.yml.dist to parameters.yml"
+cp config/parameters.yml.dist config/parameters.yml
 echo "- Building app..."
 node node_modules/@triotech/vue-core/build/build.js
 echo "- Copying .web in tmp..."
@@ -11,7 +14,4 @@ echo "- Moving tmp to web..."
 mv tmp web
 echo "- Removing old..."
 rm -rf old
-#TODO instead of simple copy, handle differences
-echo "- Copying parameters.yml.dist to parameters.yml"
-cp config/parameters.yml.dist config/parameters.yml
 echo "Successfully ran Postdeploy script !"
