ad603bbb756c78e311f7c7a0870a6c2abce04e63
rcampos <rebecca.campos@triotech.fr>
2018-02-16 14:25:51 +0100

    Added textarea autosize to vue-form-generator.js

--- src/vendor/vue-form-generator.js
+++ src/vendor/vue-form-generator.js
@@ -2,5 +2,6 @@ import VueFormGenerator from 'vue-form-generator';
 
 import 'vue-form-generator/dist/vfg.css';
 import '@triotech/vue-core/src/scss/_vfg.scss';
+import '@triotech/vue-core/src/lib/helper/autosize';
 
 export default VueFormGenerator;
