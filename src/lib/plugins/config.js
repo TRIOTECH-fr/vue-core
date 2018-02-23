869e9ba52fd092f5c7d2adfd4f9fc387e1eb95bc
qkdreyer <quentin.dreyer@triotech.fr>
2018-02-23 16:29:02 +0100

    Fixed setter method to config plugin

--- src/lib/plugins/config.js
+++ src/lib/plugins/config.js
@@ -3,18 +3,30 @@ import Vue from 'vue';
 import parameters from 'json-loader!yaml-loader!@/../config/parameters.yml';
 
 // TODO generate computed fns from parameters keys
+// TODO handle dot notation for get/set
 
 const Config = new Vue({
   computed: {
-    host: () => (localStorage.getItem('host') || parameters.host).replace(/\/$/, ''),
-    endpoint: () => ((localStorage.getItem('endpoint') || parameters.endpoint) || '').replace(/\/$/, '').concat('/'),
+    parameters: () => parameters,
+    host() {
+      return (this.local('host') || this.parameters.host || '').replace(/\/$/, '');
+    },
+    endpoint() {
+      return (this.local('endpoint') || this.parameters.endpoint || '').replace(/\/$/, '').concat('/');
+    },
   },
   methods: {
-    all: () => parameters,
-    // TODO handle dot notation for get/set
-    get: (key, fallback) => parameters[key] || fallback,
-    set: (key, value) => {
-      parameters[key] = value;
+    all() {
+      return this.parameters;
+    },
+    get(key, fallback) {
+      return this.parameters[key] || fallback;
+    },
+    set(key, value) {
+      this.$set(this.parameters, key, value);
+    },
+    local(key) {
+      return localStorage.getItem(key);
     },
   },
 });
