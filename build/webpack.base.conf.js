948a27ed0cb1f6c72ad6b287df4c6d2a128feae0
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-03-20 11:23:49 +0100

    Transpiling vuex-cache

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,7 +56,8 @@ module.exports = {
           resolve('config'),
           resolve('test'),
           resolve('node_modules/@triotech/vue-core/src'),
-          resolve('node_modules/sweet-modal-vue')
+          resolve('node_modules/sweet-modal-vue'),
+          resolve('node_modules/vuex-cache'),
         ]
       },
       {
