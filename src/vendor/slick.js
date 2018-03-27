661b602680f2e527309c6243d827c62f969c4cdc
rcampos <rebecca.campos@triotech.fr>
2018-03-27 17:40:34 +0200

    Added slick theme css

--- src/vendor/slick.js
+++ src/vendor/slick.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Slick from 'vue-slick';
-import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
 
 Vue.component('slick', Slick);
