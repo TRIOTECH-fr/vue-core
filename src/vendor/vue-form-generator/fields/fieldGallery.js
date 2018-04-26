6e4f89bb3032f5aaad93652d6ef6773a045b6a9b
rcampos <rebecca.campos@triotech.fr>
2018-04-26 13:54:20 +0200

    Added fieldGallery

---
+++ src/vendor/vue-form-generator/fields/fieldGallery.js
@@ -0,0 +1,63 @@
+import Vue from 'vue';
+import VueSelectImage from '../../vue-select-image';
+import Mixins from './mixins';
+import Ajax from '@triotech/vue-core/src/lib/plugins/ajax';
+
+Vue.component('fieldGallery', {
+  components: {
+    VueSelectImage,
+  },
+  mixins: Mixins,
+  data() {
+    return {
+      galleryModel: [],
+      dataImages: [],
+      isMultiple: this.schema.selectOptions.multiple,
+    };
+  },
+  computed: {
+    value() {
+      return this.galleryModel;
+    },
+  },
+  watch: {
+    model(newValue) {
+      if (Object.keys(newValue).length < 1) {
+        this.galleryModel = null;
+      }
+    },
+    galleryModel(newValue, oldValue) {
+      this.galleryModel = this._.isNull(newValue) ? [] : newValue;
+      this.setModelValueByPath(this.schema.model, newValue);
+    },
+  },
+  mounted() {
+    this._.each(this.schema.choices, (choice) => {
+      this.dataImages.push({
+        id: choice.id,
+        src: `${this.$config.host}/uploads${choice.url}`,
+        alt: choice.label,
+      });
+    });
+    this.galleryModel = this.modelNameToProperty(this.schema.model, this.model);
+  },
+  methods: {
+    onSelectImage(images) {
+      this.galleryModel = images.id;
+    },
+    onSelectMultipleImage(images) {
+      this._.each(images, (image) => {
+        this.galleryModel.push(image.id);
+      });
+    },
+  },
+  template: `
+    <vue-select-image
+      :dataImages="dataImages"
+      :is-multiple="isMultiple"
+      @onselectimage="onSelectImage"
+      @onselectmultipleimage="onSelectMultipleImage"
+    >
+    </vue-select-image>
+  `,
+});
