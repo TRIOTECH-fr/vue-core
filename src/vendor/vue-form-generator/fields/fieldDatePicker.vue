89aa84869652f5c93047019046bf3b204a1123d0
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-06-04 11:33:29 +0200

    Compatibility with vuejs-datepicker ^1.0.0

--- src/vendor/vue-form-generator/fields/fieldDatePicker.vue
+++ src/vendor/vue-form-generator/fields/fieldDatePicker.vue
@@ -3,13 +3,14 @@
     v-model="datePickerModel"
     :placeholder="schema.placeholder"
     :required="schema.required"
-    :language="language"
+    :language="languages[language]"
     input-class="form-control"
   />
 </template>
 
 <script>
   import DatePicker from 'vuejs-datepicker';
+  import * as languages from "vuejs-datepicker/src/locale";
   import abstractField from './abstractField';
 
   export default {
@@ -27,6 +28,7 @@
     },
     data() {
       return {
+        languages,
         datePickerModel: '',
       };
     },
