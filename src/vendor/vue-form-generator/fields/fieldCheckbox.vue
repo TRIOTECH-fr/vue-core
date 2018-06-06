1b59c671e3b50584f6ce042aba80291be6414e9e
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-06-06 14:06:53 +0200

    Overriding fieldCheckbox for required attribute

---
+++ src/vendor/vue-form-generator/fields/fieldCheckbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :id="getFieldID(schema)"
+    type="checkbox"
+    v-model="value"
+    :autocomplete="schema.autocomplete"
+    :disabled="disabled"
+    :name="schema.inputName"
+    :class="schema.fieldClasses"
+    :required="schema.required"
+  />
+</template>
+
+<script>
+  import VueFormGenerator from 'vue-form-generator';
+
+  export default {
+    ...VueFormGenerator.component.components.fieldCheckbox,
+  };
+</script>
