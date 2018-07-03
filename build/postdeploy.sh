a50a7247d7ecc5e0806a7638a6ea13053f5ef7cb
qkdreyer <quentin.dreyer@triotech.fr>
2018-07-03 11:33:17 +0200

    Added exit codes in postdeploy script

--- build/postdeploy.sh
+++ build/postdeploy.sh
@@ -2,15 +2,15 @@
 clear
 echo "Starting Postdeploy Script :"
 echo "- Updating config/parameters.yml file..."
-node node_modules/@triotech/vue-core/build/parameters.js
+node node_modules/@triotech/vue-core/build/parameters.js || exit 1
 echo "- Building application..."
-node node_modules/@triotech/vue-core/build/build.js
+node node_modules/@triotech/vue-core/build/build.js || exit 2
 echo "- Copying .web folder in tmp folder..."
-rsync -r .web/ tmp
+rsync -r .web/ tmp || exit 3
 echo "- Moving web folder to old folder..."
-mv web old
+mv web old || exit 4
 echo "- Moving tmp folder to web folder..."
-mv tmp web
+mv tmp web || exit 5
 echo "- Removing old folder..."
-rm -rf old
+rm -rf old || exit 6
 echo "Successfully ran Postdeploy script !"
