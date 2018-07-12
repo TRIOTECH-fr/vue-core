d44e8dd3f880b2ea3b49482c97958f7d3116eca4
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-07-12 10:09:09 +0200

    Reducing bundle size by excluding vue-awesome icons

--- src/vendor/vue-awesome.js
+++ src/vendor/vue-awesome.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
-import VueAwesome from 'vue-awesome';
 import Icon from 'vue-awesome/components/Icon';
 
 Vue.component('icon', Icon);
 
-export default VueAwesome;
+export default Icon;
