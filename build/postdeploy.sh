06316fb229acb7c6f415e7004594e74fad62616a
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-07-13 09:33:48 +0200

    Ensuring folder existence in postdeploy script

--- build/postdeploy.sh
+++ build/postdeploy.sh
@@ -7,10 +7,8 @@ echo "- Building application..."
 node node_modules/@triotech/vue-core/build/build.js || exit 2
 echo "- Copying .web folder in tmp folder..."
 rsync -r .web/ tmp || exit 3
-echo "- Moving web folder to old folder..."
-mv web old || exit 4
+[[ -d web ]] && (echo "- Moving web folder to old folder..." && mv web old || exit 4)
 echo "- Moving tmp folder to web folder..."
 mv tmp web || exit 5
-echo "- Removing old folder..."
-rm -rf old || exit 6
+[[ -d old ]] && (echo "- Removing old folder..." && rm -rf old || exit 6)
 echo "Successfully ran Postdeploy script !"
