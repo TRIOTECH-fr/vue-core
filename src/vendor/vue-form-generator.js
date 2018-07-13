be82ea9bea38c66ee3c5c3d2c14f5f9909fcf75b
rcampos <rebecca.campos@triotech.fr>
2018-07-13 14:45:12 +0200

    Remove import 'vue-form-generator/dist/vfg.css' from vue-form-genrator.js

--- src/vendor/vue-form-generator.js
+++ src/vendor/vue-form-generator.js
@@ -1,5 +1,4 @@
 import VueFormGenerator from 'vue-form-generator';
-import 'vue-form-generator/dist/vfg.css';
 import autoload from '../lib/core/autoload';
 import './vue-autosize';
 import '../scss/_vfg.scss';
