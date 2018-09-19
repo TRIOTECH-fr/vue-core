00f172cfe8bd890a27dbca0644d93f0e11571db6
qkdreyer <quentin.dreyer@triotech.fr>
2018-09-19 17:14:24 +0200

    style: format .commitlintrc.js

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+// https://marionebl.github.io/commitlint/#/reference-configuration
+
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+  ],
+};
