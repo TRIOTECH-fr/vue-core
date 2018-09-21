56329ad1daa599aa7fe0533d7da4dccbc8011c3d
Quentin DREYER <quentin.dreyer@triotech.fr>
2018-09-21 11:27:16 +0200

    fix(ajax): add extra form validation errors

--- src/mixins/AbstractAjax.js
+++ src/mixins/AbstractAjax.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     catchHandler(error) {
       const { status, data } = error.response;
-      const text = status === 400 ? _.reduce(_.transform(data.errors.children, (carry, value, key) => {
+      let text = status === 400 ? _.reduce(_.transform(data.errors.children, (carry, value, key) => {
         if (value.errors) {
           carry[key] = value.errors;
         }
@@ -11,6 +11,10 @@ export default {
         carry.push(`${this.$t(`flashes.${this.name}.error_${status}.${field}`)} : ${errors.join()}`);
         return carry;
       }, []).join('\n') : this.$t(`flashes.${this.name}.error_${status}`);
+
+      if (_.isEmpty(text)) {
+        text = `${this.$t(`flashes.${this.name}.error_${status}.extra`)} : ${data.errors.errors.join('\n')}`;
+      }
 
       this.$notify({
         title: this.$t(`flashes.${this.name}.error_${status}_title`),
