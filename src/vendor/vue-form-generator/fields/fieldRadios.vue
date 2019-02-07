652cb4cd752c7a8a33fb46b3ea728d63e39a1116
Rébecca CAMPOS <rebecca.campos@triotech.fr>
2019-02-07 15:43:06 +0100

    fix: can disable input radio

--- src/vendor/vue-form-generator/fields/fieldRadios.vue
+++ src/vendor/vue-form-generator/fields/fieldRadios.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="radio-list" :class="schema.listClasses" :disabled="disabled">
-    <label v-for="item in items" :class="{'is-checked': isItemChecked(item)}">
+  <div :class="schema.listClasses" :disabled="disabled" class="radio-list">
+    <label v-for="(item, idx) in items" :key="idx" :class="{'is-checked': isItemChecked(item)}">
       <input
         v-attributes="'input'"
         :id="getFieldID(schema)"
-        :disabled="disabled"
+        :disabled="isItemDisabled(item)"
         :name="id"
         :value="getItemValue(item)"
         :checked="isItemChecked(item)"
@@ -12,7 +12,7 @@
         :required="schema.required"
         type="radio"
         @click="onSelection(item)"
-      />
+      >
       <span>{{ getItemName(item) }}</span>
     </label>
   </div>
